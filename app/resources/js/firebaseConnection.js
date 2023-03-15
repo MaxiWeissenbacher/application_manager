ac097d22c02c9a0aa3e4cc98ded8eaf6a86f32df
MaxiWeissenbacher <46745783+MaxiWeissenbacher@users.noreply.github.com>
2023-03-15 10:49:06 +0100

    Update firebaseConnection.js

--- app/resources/js/firebaseConnection.js
+++ app/resources/js/firebaseConnection.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAjW7cQ9Ng9zsLfFNYqOW7TpLaQcaQWE-g",
-    authDomain: "bewerbungsmanager-f8796.firebaseapp.com",
-    databaseURL: "https://bewerbungsmanager-f8796-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bewerbungsmanager-f8796",
-    storageBucket: "bewerbungsmanager-f8796.appspot.com",
-    messagingSenderId: "542771510200",
-    appId: "1:542771510200:web:c0a1592f855ce18e73ed4d",
+    apiKey: "YOURAPIKEY",
+    authDomain: "YOURDOMAIN",
+    databaseURL: "YOURDATABASEURL",
+    projectId: "YOURPROJECTID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOURMESSAGINGID",
+    appId: "YOURAPPID",
 };
 
 // eslint-disable-next-line no-undef
